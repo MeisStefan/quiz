83389fdce083c35c41fc6e36fa964673a9928793
MeisStefan <stefanbratu55@gmail.com>
2024-11-20 20:04:17 +0200

    structura proiect

---
+++ sources/controller.py
@@ -0,0 +1,49 @@
+from tkinter import *
+
+
+def start_quiz():
+    root = Tk()
+    root.title("General Knowledge Quiz")
+    root.geometry("500x150")
+    root.mainloop()
+    myLabel
+
+def update_timer():
+    # TODO
+    pass
+
+
+def disable_buttons():
+    # TODO
+    pass
+
+
+def display_question():
+    # TODO
+    pass
+
+
+def display_result():
+    # TODO
+    pass
+
+
+def select_answer():
+    # TODO
+    pass
+
+
+def next_question():
+    # TODO
+    pass
+
+
+def restart_quiz():
+    # TODO
+    pass
+
+
+def exit_quiz():
+    # TODO
+    pass
+
