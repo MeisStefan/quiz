8c812e6d8aa0fa63c746bd6546a41d8cd2b3c600
MeisStefan <stefanbratu55@gmail.com>
2024-11-24 15:09:58 +0200

    refactoring

--- sources/controller.py
+++ sources/controller.py
@@ -1,21 +1,13 @@
 from tkinter import *
 
+class QuizInterface:
+    def __init__(self, label, button):
+        self.label = label
+        self.button = button
+
 
 def start_quiz():
-    root = Tk()
-    root.title("General Knowledge Quiz")
-    root.geometry("500x150")
-    myLabel = Label(root, text="General Knowledge Quiz", font="Arial 25")
-    myLabel.pack()
-    myButton = Button(root, text="Start", font="Arial 25")
-    myButton.pack()
-    menubar = Menu(root)
-    root.config(menu=menubar)
-    file_menu = Menu(menubar, tearoff=False)
-    file_menu.add_command(label='Exit', command=root.destroy)
-    file_menu.add_command(label='Restart')
-    menubar.add_cascade(label="Menu", menu=file_menu)
-    root.mainloop()
+    # TODO
     pass
 
 def update_timer():
@@ -56,4 +48,3 @@ def restart_quiz():
 def exit_quiz():
     # TODO
     pass
-
--- sources/main.py
+++ sources/main.py
@@ -1,3 +1,21 @@
+from tkinter import *
 from controller import *
 
-start_quiz()
+def createInterface(root):
+    root.title("General Knowledge Quiz")
+    root.geometry("500x150")
+    myLabel = Label(root, text="General Knowledge Quiz", font="Arial 25")
+    myLabel.pack()
+    myButton = Button(root, text="Start", font="Arial 25")
+    myButton.pack()
+    menubar = Menu(root)
+    root.config(menu=menubar)
+    file_menu = Menu(menubar, tearoff=False)
+    file_menu.add_command(label='Exit', command=root.destroy)
+    file_menu.add_command(label='Restart', command=restart_quiz)
+    menubar.add_cascade(label="Menu", menu=file_menu)
+
+root = Tk()
+createInterface(root)
+root.mainloop()
+
