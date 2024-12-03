ec0c3b71ce40503003747dd152fc5faa595f3a3e
MeisStefan <stefanbratu55@gmail.com>
2024-12-03 18:39:29 +0200

    I made a new question(almost)

--- sources/controller.py
+++ sources/controller.py
@@ -1,8 +1,12 @@
+from tkinter.constants import DISABLED
+
+from model import *
+
 current_question = 0
 
 questions = ["Care este capitala Egiptului?", "Cand a inceput al doilea Razboi Mondial?", "Cine este Fernando Magellan?", "Cand a avut loc revolutia din '89?"]
 
-options = ["Cairo, Paris, Berlin, Roma", "1954, 1345, 1939, 1945","Un navigator, Un extremist, Un lider comunist, Un lider fascist", "88, 1979, 2000, '89"]
+options = ["Cairo", "Paris", "Berlin", "Roma", "1954", "1345", "1939", "1945","Un navigator", "Un extremist", "Un lider comunist", "Un lider fascist", "88", "1979", "2000", "89"]
 
 correct_answers = ["Cairo", "1939", "Un navigator", "89"]
 
@@ -10,30 +14,51 @@ correct_answers_index = [1, 3, 1, 4]
 
 
 def disable_buttons():
-
-    pass
+   global buttons_list
+   for button in buttons_list:
+        button.config(state=DISABLED)
 
 
 def display_question():
+    next_question()
+    global current_question
+    global options
+    start = 4 * current_question
+    end = start + 4
+    current_question_options = options[start:end:]
+    print(current_question_options)
 
-    pass
+    global buttons_list
+    for i in range(4):
+        current_button = buttons_list[i]
+        current_button.config(text = current_question_options[i])
+
+    # schimb numele intrebarii
+
+
+    # reactivez butoanele
+    # decolorez toate butoanele
+
 
 
 def display_result():
-    # TODO
+
     pass
 
 
 def select_answer(answer):
+    global buttons_list
+    selected_button = buttons_list[answer - 1]
     if answer == correct_answers_index[current_question]:
-        print("correct")
+        selected_button.config(disabledforeground="green")
     else:
-        print("wrong")
+        selected_button.config(disabledforeground="red")
+    disable_buttons()
+    selected_button.after(2000, display_question)
 
 def next_question():
     global current_question
     current_question += 1
-    pass
 
 
 def restart_quiz():
