f59d4979d3bdae5bb1fe9cda530cca3d093f6181
MeisStefan <stefanbratu55@gmail.com>
2024-12-08 16:42:25 +0200

    refactoring pagina de pornire

--- sources/main.py
+++ sources/main.py
@@ -5,15 +5,20 @@ from controller import *
 from model import *
 
 from tkinter import *
+
+
+def create_start_screen(root, quiz_model):
+    startLabel = Label(root, text="Christmas Quiz", font="Arial 25")
+    quiz_model.store_start_label(startLabel)
+    startLabel.pack()
+    startButton = Button(root, text="Start", font="Arial 25", command=lambda: start_quiz(quiz_model, root))
+    quiz_model.store_start_button(startButton)
+    startButton.pack()
+
 root = Tk()
 root.title("Christmas Quiz")
 root.geometry("900x300")
-startLabel = Label(root, text="Christmas Quiz", font="Arial 25")
-quiz_model.store_start_label(startLabel)
-startLabel.pack()
-startButton = Button(root, text="Start", font="Arial 25", command=lambda: start_quiz(quiz_model, root))
-quiz_model.store_start_button(startButton)
-startButton.pack()
+create_start_screen(root, quiz_model)
 menubar = Menu(root)
 root.config(menu=menubar)
 file_menu = Menu(menubar, tearoff=False)
