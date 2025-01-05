0d36a3125060793c8cd80635f83a441dee706cf5
MeisStefan <stefanbratu55@gmail.com>
2025-01-05 14:25:23 +0200

    refactoring rename mainWindow

--- sources/main.py
+++ sources/main.py
@@ -6,16 +6,15 @@ from model import *
 
 from tkinter import *
 
-
-root = Tk()
-root.title("Christmas Quiz")
-root.geometry("900x300")
-create_start_screen(root, quiz_model)
-menubar = Menu(root)
-root.config(menu=menubar)
+mainWindow = Tk()
+mainWindow.title("Christmas Quiz")
+mainWindow.geometry("900x300")
+create_start_screen(mainWindow, quiz_model)
+menubar = Menu(mainWindow)
+mainWindow.config(menu=menubar)
 file_menu = Menu(menubar, tearoff=False)
-file_menu.add_command(label='Exit', command=root.destroy)
-file_menu.add_command(label='Restart', command=lambda: restart_quiz(root, quiz_model))
+file_menu.add_command(label='Exit', command=mainWindow.destroy)
+file_menu.add_command(label='Restart', command=lambda: restart_quiz(mainWindow, quiz_model))
 menubar.add_cascade(label="Menu", menu=file_menu)
 
-root.mainloop()
+mainWindow.mainloop()
