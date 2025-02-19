b968cb002576abec4fc92866cd5cd247c4ca0e07
MeisStefan <stefanbratu55@gmail.com>
2025-02-19 20:05:43 +0200

    am afisat toate intrebarile cu toate raspunsurile

--- sources/admin_page.py
+++ sources/admin_page.py
@@ -5,24 +5,28 @@ def create_admin_page(quiz_model):
     admin_window.title("Admin page")
     admin_window.geometry("900x900")
     quiz_questions = quiz_model.questions
-    original_question = quiz_questions[0]
-    display_quiz_question(quiz_model, admin_window, original_question)
-
+    for i in range(len(quiz_questions)):
+        original_question = quiz_questions[i]
+        display_quiz_question(quiz_model, admin_window, original_question, original_question_index=i)
     new_question_Button = Button(admin_window, text="Press for new question", font="Bolt 25", command=lambda:create_new_question(admin_window, new_question_Button))
-    new_question_Button.grid(row=4, column=0, columnspan=3)
+    new_question_Button.grid(row=len(quiz_questions) * 4, column=0, columnspan=3)
 
-def display_quiz_question(quiz_model, admin_window, original_question):
-    explicative_label_modified_question = Label(admin_window, text="Intrebarea 1:", font="Arial 25")
-    explicative_label_modified_question.grid(row=0, column=0)
+def display_quiz_question(quiz_model, admin_window, original_question, original_question_index):
+    label_question_index = str(original_question_index + 1)
+    explicative_label_modified_question = Label(admin_window, text="Intrebarea " + label_question_index + ":", font="Arial 25")
+    explicative_label_modified_question.grid(row=original_question_index * 4, column=0)
     question_title_Entry = Entry(admin_window, font="Arial 25", bd=5, fg="black", width=20)
     question_title_Entry.insert(0, original_question)
-    question_title_Entry.grid(row=0, column=1)
+    question_title_Entry.grid(row=original_question_index * 4, column=1)
     quiz_options = quiz_model.options
-    original_answers = quiz_options[0:4:1]
+    number_of_options = 4
+    start = number_of_options * original_question_index
+    end = start + number_of_options
+    original_answers = quiz_options[start:end:]
     for i in range(4):
         answer_option_Entry = Entry(admin_window, font="Arial 25", bd=5, fg="black", width=20)
         answer_option_Entry.insert(0, original_answers[i])
-        answer_option_Entry.grid(row=i, column=2)
+        answer_option_Entry.grid(row=original_question_index * 4 + i, column=2)
 
 def create_new_question(admin_window, new_question_button):
     explicative_label_new_question = Label(admin_window, text="New question & answers:", font="Arial 25")
